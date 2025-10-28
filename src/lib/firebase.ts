a2d397bad4cd45edb427ffdf54492fa23e807883
Stan Chen <43565411+stancsz@users.noreply.github.com>
2025-10-28 13:44:12 +0000

    Edited Spark

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
   apiKey: "AIzaSyDfViiG8mhz-uGDIIEeMaiXB8_ww0mMWx4",
   authDomain: "prompt-engineering-l.firebaseapp.com",
   projectId: "prompt-engineering-l",
   storageBucket: "prompt-engineering-l.firebasestorage.app",
-
-
+  messagingSenderId: "1023125908531",
+  appId: "1:1023125908531:web:876c1bb04d7962ce120746",
+  measurementId: "G-N04WCLPJGE"
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
