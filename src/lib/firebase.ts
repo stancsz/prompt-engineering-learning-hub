73c99fe9d6f97039baaa56433b0af1eb8f8d584f
Stan Chen <43565411+stancsz@users.noreply.github.com>
2025-10-28 13:07:07 +0000

    Edited Spark

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || '',
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || '',
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || '',
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '',
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || ''
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_
 }
-
 const app = initializeApp(firebaseConfig)
+e
 
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+
+
+
+
