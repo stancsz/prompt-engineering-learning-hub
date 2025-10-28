68eeb5a72ac4027acc5ee81c1d670f6e4c873fa3
Stan Chen <43565411+stancsz@users.noreply.github.com>
2025-10-28 13:21:49 +0000

    Edited Spark

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+const firebaseConfig = {
 
 const firebaseConfig = {
   apiKey: "AIzaSyBXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
@@ -10,8 +10,8 @@ const firebaseConfig = {
   messagingSenderId: "1023125908531",
   appId: "1:1023125908531:web:876c1bb04d7962ce120746",
   measurementId: "G-N04WCLPJGE"
-};
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
+
