c810dac7848b032327bb003179f814eac58da8f5
Stan Chen <43565411+stancsz@users.noreply.github.com>
2025-10-28 13:36:24 +0000

    Edited Spark

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-  projectId: "prompt-eng
+const firebaseConfig = {
+  apiKey: "AIzaSyB7qZ6x_9Z8Z6Z8Z6Z8Z6Z8Z6Z8Z6Z8Z6Z",
+  authDomain: "prompt-eng.firebaseapp.com",
+  projectId: "prompt-eng",
+  storageBucket: "prompt-eng.appspot.com",
   messagingSenderId: "1023125908531",
+  appId: "1:1023125908531:web:abcdef1234567890",
   measurementId: "G-N04WCLPJGE"
+};
 
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-
-
-
-
-
-
-
